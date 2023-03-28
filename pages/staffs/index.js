3d6c8f43a07b8dd3493680eb056c680a865c9541
Tohda <tohda@users.noreply.github.com>
2023-03-28 11:10:38 +0000

    リンク追加

--- pages/staffs/index.js
+++ pages/staffs/index.js
@@ -1,14 +1,29 @@
-import StaffLayout from '../../components/staffLayout'
-import Head from 'next/head'
+import StaffLayout from "../../components/staffLayout";
+import Head from "next/head";
 import { createMicrocmsClient } from "../../lib/microcmsClient";
 import { LiffContext } from "../_app";
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from "react";
 import { createReservation, getReservations } from "../../lib/useReservations";
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Button, Snackbar, Alert } from '@mui/material';
-import { red, grey } from '@mui/material/colors';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import { red, grey } from "@mui/material/colors";
 import { lineNotify } from "../../lib/lineNotify";
-import { fetchThisWeeks, isIncludeWorkday, getReservation, createReservationData } from '../../lib/util'
-import { useRouter } from 'next/router'
+import {
+  fetchThisWeeks,
+  isIncludeWorkday,
+  getReservation,
+  createReservationData,
+} from "../../lib/util";
+import { useRouter } from "next/router";
 
 var weekJp = ["日", "月", "火", "水", "木", "金", "土"];
 
@@ -22,42 +37,48 @@ export default function Staff({ serviceDomain, microcmsApiKey }) {
     serviceDomain: serviceDomain,
     apiKey: microcmsApiKey,
   });
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    const { id } = router.query
-    if(!id)return ;
-    client.get({
-      endpoint: `staffs/${id}`
-    }).then((content) => {
-      setStaff(content)
-      getReservations(client, `staff[equals]${staff?.id}`).then((_reservations) => {
-        setReservations(_reservations)
+    const { id } = router.query;
+    if (!id) return;
+    client
+      .get({
+        endpoint: `staffs/${id}`,
       })
-    })
-  }, [router])
+      .then((content) => {
+        setStaff(content);
+        getReservations(client, `staff[equals]${staff?.id}`).then(
+          (_reservations) => {
+            setReservations(_reservations);
+          }
+        );
+      });
+  }, [router]);
 
   useEffect(() => {
-    getReservations(client, `staff[equals]${staff?.id}`).then((_reservations) => {
-      setReservations(_reservations)
-    })
-  }, [staff])
+    getReservations(client, `staff[equals]${staff?.id}`).then(
+      (_reservations) => {
+        setReservations(_reservations);
+      }
+    );
+  }, [staff]);
 
-  if(!staff || !reservations) {
-    return <></>
+  if (!staff || !reservations) {
+    return <></>;
   }
   const workdays = staff.workdays.map((e) => new Date(e.workday));
   const dates = fetchThisWeeks();
   const reserve = (date, staffId, profile) => {
-    const reservation = createReservationData(date, staffId, profile)
+    const reservation = createReservationData(date, staffId, profile);
     createReservation(client, reservation, staff, () => {
-      setReservations([...reservations, reservation])
-      const date = new Date(reservation.reservationAt).toLocaleString()
-      const message = `${staff.staffName}さん：${reservation.userName}様の${date}から予約されました。`
-      const userMessage = `${date}の予約をしました`
-      lineNotify(message)
-      setSnackMessage(userMessage)
-    })
-  }
+      setReservations([...reservations, reservation]);
+      const date = new Date(reservation.reservationAt).toLocaleString();
+      const message = `${staff.staffName}さん：${reservation.userName}様の${date}から予約されました。`;
+      const userMessage = `${date}の予約をしました`;
+      lineNotify(message);
+      setSnackMessage(userMessage);
+    });
+  };
 
   return (
     <StaffLayout staff={staff}>
@@ -69,49 +90,84 @@ export default function Staff({ serviceDomain, microcmsApiKey }) {
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
-                <TableCell>
-                  時間
-                </TableCell>
+                <TableCell>時間</TableCell>
                 {dates.map((workday) => {
-                  return <TableCell key={`week-${workday.toISOString()}`}>
-                    {workday.getMonth()+1}/{workday.getDate()}({weekJp[workday.getDay()]})
-                  </TableCell>
+                  return (
+                    <TableCell key={`week-${workday.toISOString()}`}>
+                      {workday.getMonth() + 1}/{workday.getDate()}(
+                      {weekJp[workday.getDay()]})
+                    </TableCell>
+                  );
                 })}
               </TableRow>
             </TableHead>
             <TableBody>
               {[...Array(12)].map((_, _hour) => {
-                const hour = _hour + 8
+                const hour = _hour + 8;
                 return (
                   <TableRow key={`staff-${hour}`}>
                     <TableCell key={`hour-${hour}`}>
                       {hour}:00 - {hour + 1}:00
                     </TableCell>
                     {dates.map((workday) => {
-                      const _date = new Date(workday.getFullYear(), workday.getMonth(), workday.getDate(), hour);
-                      const isWorking = isIncludeWorkday(workdays, workday, hour);
-                      const reservation = getReservation(reservations, workday, hour);
-                      return isWorking ?
-                        reservation ?
-                          <TableCell key={`week-${workday.toISOString()}-${hour}`} sx={{ bgcolor: red[500], color: grey[50] }} align='center' >
-                            {/* {
+                      const _date = new Date(
+                        workday.getFullYear(),
+                        workday.getMonth(),
+                        workday.getDate(),
+                        hour
+                      );
+                      const isWorking = isIncludeWorkday(
+                        workdays,
+                        workday,
+                        hour
+                      );
+                      const reservation = getReservation(
+                        reservations,
+                        workday,
+                        hour
+                      );
+                      return isWorking ? (
+                        reservation ? (
+                          <TableCell
+                            key={`week-${workday.toISOString()}-${hour}`}
+                            sx={{ bgcolor: red[500], color: grey[50] }}
+                            align="center"
+                          >
+                            {
                               // workshop: LINE のプロフィールの userId と microCMS で予約している lineId が同じ場合はリンクを表示する
-                              profile?.userId == reservation.lineId ?
-                                <Button href={`/reservations/${reservation.id}`}>予約確認</Button> :
-                                '予約済み'
-                            } */}
+                              profile?.userId == reservation.lineId ? (
+                                <Button
+                                  href={`/reservations/${reservation.id}`}
+                                >
+                                  予約確認
+                                </Button>
+                              ) : (
+                                "予約済み"
+                              )
+                            }
                             予約済み
-                          </TableCell> :
-                          <TableCell key={`week-${workday.toISOString()}-${hour}`} align='center'>
-                              <Button
-                                variant="text"
-                                onClick={() => { reserve(_date, staff.id, profile) }}
-                              >
-                                予約
-                              </Button>
-                          </TableCell> :
-                        <TableCell key={`week-${workday.toISOString()}-${hour}`} sx={{ bgcolor: grey[500] }}>
-                        </TableCell>
+                          </TableCell>
+                        ) : (
+                          <TableCell
+                            key={`week-${workday.toISOString()}-${hour}`}
+                            align="center"
+                          >
+                            <Button
+                              variant="text"
+                              onClick={() => {
+                                reserve(_date, staff.id, profile);
+                              }}
+                            >
+                              予約
+                            </Button>
+                          </TableCell>
+                        )
+                      ) : (
+                        <TableCell
+                          key={`week-${workday.toISOString()}-${hour}`}
+                          sx={{ bgcolor: grey[500] }}
+                        ></TableCell>
+                      );
                     })}
                   </TableRow>
                 );
@@ -123,18 +179,23 @@ export default function Staff({ serviceDomain, microcmsApiKey }) {
           <Snackbar
             open={!!snackMessage}
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
+              vertical: "bottom",
+              horizontal: "right",
             }}
           >
-            <Alert onClose={()=>{setSnackMessage(undefined)}} severity="success">
+            <Alert
+              onClose={() => {
+                setSnackMessage(undefined);
+              }}
+              severity="success"
+            >
               {snackMessage}
             </Alert>
           </Snackbar>
         }
       </article>
     </StaffLayout>
-  )
+  );
 }
 
 export async function getStaticProps() {
@@ -142,7 +203,7 @@ export async function getStaticProps() {
     props: {
       liffId: process.env.LIFF_ID,
       serviceDomain: process.env.SERVICE_DOMAIN,
-      microcmsApiKey: process.env.MICROCMS_API_KEY
-    }
-  }
+      microcmsApiKey: process.env.MICROCMS_API_KEY,
+    },
+  };
 }
